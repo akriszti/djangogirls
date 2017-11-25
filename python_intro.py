c45c18e216677ead2506a17203a0c5229fcc601c
akriszti <kriszti.asztalos@yahoo.com>
2017-11-25 14:37:43 +0100

    Post model created

---
+++ python_intro.py
@@ -0,0 +1,16 @@
+print('Hello!')
+
+city = 'Debrecen'
+if city == 'Becs':
+    print('Becs')
+elif city == 'Budapest':
+    print('Budapest')
+elif city == 'Debrecen':
+    print('yay!')
+else:
+    print ('unknown city')
+
+def hi(name, name2):
+    print('Szia ' + name + name2 + '!')
+
+hi('Django', 'Girl')
